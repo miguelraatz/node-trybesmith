529133ec1e88329c1f4e1d57560cb3eaffba24a2
Miguel Raatz <miguelraatz_@hotmail.com>
2023-05-26 00:27:28 -0300

    feat: implement middlewares for name and price for create product on route products - requirement 5

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productController from '../controllers/product.controller';
+import validateName from '../middlewares/validateName.middleware';
+import validatePrice from '../middlewares/validatePrice.middleware';
 
 const productRouter = Router();
 
-productRouter.post('/', productController.createProduct);
+productRouter.post('/', validateName, validatePrice, productController.createProduct);
 
 productRouter.get('/', productController.getProducts);
 
